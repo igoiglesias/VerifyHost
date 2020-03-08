8a4fce8aa5b013368cd9a72d9a6a951af8c85303
igoiglesias <contato@igoriglesias.com>
2020-03-08 08:24:53 -0300

    Inicio da migração para Vuejs

--- js/myvue.js
+++ js/myvue.js
@@ -1,13 +1,50 @@
-const ping = new Vue({
-    el:"#ping",
+Vue.component('tab-home',{
+    template: `<h2>Bem vindo ao VHost</h2>`
+})
+Vue.component("tab-ping",{
+    template: `<div class="row">
+            <div class="col-md-12">
+              <h1 class="text-center">Ping</h1>
+            </div>
+          </div>
+          <div class="row text-center">
+            <div class="col-md-4 text-center"></div>
+            <div class="col-md-4">
+              <form>
+                <div class="form-group" >
+                  <label class="control-label">IP/Site</label>
+                  <input class="form-control" id="exampleInputEmail1" placeholder="8.8.8.8"
+                  type="text" name="ip">
+                </div>
+                <div class="form-group">
+                  <label class="control-label">Qtd</label>
+                  <input class="form-control" id="exampleInputPassword1" placeholder="5"
+                  type="text" name="n">
+                </div>
+                <button type="submit" class="btn btn-lg btn-primary">Pingar</button>
+
+              </form>
+
+            </div>
+            <div class="col-md-4"></div>
+          </div>`
+})
+Vue.component("tab-tracert",{
+    template: "<h1>Tracert</h1>"
+})
+
+const app = new Vue({
+    el:"#vhost",
     data: {
-        title: "Ping"
+        title: "Ping",
+        currentTab: "Home",
+        tabs:["Home","Ping","Tracert"]
     },
-    methods:{
-        pingar: function(event){
-            event.preventDefault();
-            alert("pingando...");
-        },
+    computed: {
+        tabAtual: function(){
+            //alert("tab-" + this.currentTab.toLowerCase());
+            return "tab-" + this.currentTab.toLowerCase();
+        }
     }
 
 });
