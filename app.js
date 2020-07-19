0c1770817c700bd33438f51b55a36ac8aa5c2103
Igor Iglesias <contato@igoriglesias.com>
2020-07-19 17:32:27 -0300

    Basic functionality working

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 const form = document.forms["teste"];
 form["testar"].addEventListener("click", GetTest);
 
-function GetTest(){
+const result = document.getElementById("result");
+
+function GetTest() {
   event.preventDefault();
 
-  let ip = form["ip"].value ? form["ip"].value : "8.8.8.8";
-  let nPing = form["nping"].value ? form["nping"].value : 4;
+  let ip = form["ip"].value == "" ? "8.8.8.8" : form["ip"].value;
+  let nPing = form["nping"].value == "" ? "4" : form["nping"].value;
+
+
   DoPing(ip, nPing);
-  
+
 }
 
-function DoPing(ip, n) {
-  console.log(ip + " " + n);
+async function DoPing(ip, n) {
+
+  const Url = "App/PingController.php";
+  let Data = {
+    ip: ip,
+    n: n
+  }
+  Data = JSON.stringify(Data);
+
+  const Options = {
+    headers: {
+      "content-type": "/application/json; charset=UTF-8"
+    },
+    body: Data,
+    method: "POST"
+
+  };
+
+  await fetch(Url, Options)
+    .then(res => res.json())
+    .then(data => {
+
+      let div = document.createElement("div");
+      let h = document.createElement("h3");
+      let br = document.createElement("br");
+      let small = document.createElement("small");
+      let textH = document.createTextNode("O teste para o IP " + data["ip"] + " teve o seguinte resultado: " + data["status"]);
+      let textSmall = document.createTextNode(data["saida"]);
+
+      h.appendChild(textH);
+      small.appendChild(textSmall);
+      div.appendChild(h);
+      div.appendChild(br);
+      div.appendChild(small);
+      
+      result.prepend(div);
+
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+    });
+
+
+
 }
